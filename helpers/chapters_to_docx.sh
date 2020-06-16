8bdfb189ba86a97226aa4a5328670f3a5a3f0351
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2020-06-16 17:23:47 +0100

    upd

--- helpers/chapters_to_docx.sh
+++ helpers/chapters_to_docx.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for i in ./manuscript/tex/[0-9][0-9]*.tex; do
-    ./helpers/tex_to_docx.sh "$i"
+org_options='#+OPTIONS: toc:nil author:nil d:nil ^:{}'
+
+for i in ./manuscript/org/[0][0-2]*.org; do
+    name=$(basename "$i" .org)
+    dest=./manuscript/docx/"$name".docx
+
+    cat "$i" |\
+    grep -v '^[:#] -*-' |\
+    # append export options
+    sed 's/^#+TITLE.*$/&\n'"$org_options"'\n/' |\
+    grep -vE '^#\+TITLE' |\
+    # strip task counter [0/2]
+    perl -0777 -pe "s/(\n\\* +.*?) +\[[[0-9\/]+\]\n/\1\n/" |\
+    pandoc -f org+smart -t docx --top-level-division=chapter > "$dest"
 done
-
-./helpers/tex_to_docx.sh ./manuscript/tex/foreword.tex
-
-./helpers/tex_to_docx.sh ./manuscript/tex/preface.tex
--- helpers/chapters_to_tex.sh
+++ helpers/chapters_to_tex.sh
@@ -2,14 +2,15 @@
 
 org_options='#+OPTIONS: toc:nil author:nil d:nil ^:{}'
 
-for i in ./manuscript/org/[0-9][0-9]*.org; do
+for i in ./manuscript/org/[0][0-2]*.org; do
     name=$(basename "$i" .org)
     dest=./manuscript/tex/"$name".tex
 
     cat "$i" |\
+    grep -v '^[:#] -*-' |\
     # append export options
     sed 's/^#+TITLE.*$/&\n'"$org_options"'\n/' |\
     # strip task counter [0/2]
     perl -0777 -pe "s/(\n\\* +.*?) +\[[[0-9\/]+\]\n/\1\n/" |\
-    pandoc -f org -t latex --top-level-division=chapter > "$dest"
+    pandoc -f org+smart -t latex --top-level-division=chapter > "$dest"
 done
